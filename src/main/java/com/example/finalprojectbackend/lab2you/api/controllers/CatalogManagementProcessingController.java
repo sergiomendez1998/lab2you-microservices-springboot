8c461c9c21a115386aa8da2cb390a3fbf8c05edb
DannyGalvan <cgalvan29111999@gmail.com>
2023-09-05 18:35:28 -0600

    fixing security

--- src/main/java/com/example/finalprojectbackend/lab2you/api/controllers/CatalogManagementProcessingController.java
+++ src/main/java/com/example/finalprojectbackend/lab2you/api/controllers/CatalogManagementProcessingController.java
@@ -2,7 +2,6 @@ package com.example.finalprojectbackend.lab2you.api.controllers;
 
 import com.example.finalprojectbackend.lab2you.db.model.wrappers.CatalogWrapper;
 import com.example.finalprojectbackend.lab2you.service.catalogservice.AnalysisDocumentTypeService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +17,6 @@ public class CatalogManagementProcessingController {
 
     private final AnalysisDocumentTypeService analysisDocumentTypeService;
 
-    @Autowired
     public CatalogManagementProcessingController(AnalysisDocumentTypeService analysisDocumentTypeService) {
         this.analysisDocumentTypeService = analysisDocumentTypeService;
     }
