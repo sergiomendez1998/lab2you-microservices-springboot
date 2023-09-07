4dde551c8d891668100a3d622d82810eb17d0b90
sergio mendez <sergiomendezgzalez@gmail.com>
2023-09-06 19:46:09 -0600

    removing autowired from controller class

--- src/main/java/com/example/finalprojectbackend/lab2you/api/controllers/CatalogManagementProcessingController.java
+++ src/main/java/com/example/finalprojectbackend/lab2you/api/controllers/CatalogManagementProcessingController.java
@@ -2,7 +2,6 @@ package com.example.finalprojectbackend.lab2you.api.controllers;
 
 import com.example.finalprojectbackend.lab2you.db.model.wrappers.CatalogWrapper;
 import com.example.finalprojectbackend.lab2you.service.catalogservice.AnalysisDocumentTypeService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +16,6 @@ import java.util.stream.Collectors;
 public class CatalogManagementProcessingController {
 
     private final AnalysisDocumentTypeService analysisDocumentTypeService;
-
 
     public CatalogManagementProcessingController(AnalysisDocumentTypeService analysisDocumentTypeService) {
         this.analysisDocumentTypeService = analysisDocumentTypeService;
